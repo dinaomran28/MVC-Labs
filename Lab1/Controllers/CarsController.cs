e2332cec3f9d697c98d664bef4b52991f8e3d1ac
dinaomran28 <dinaomran8@gmail.com>
2023-03-11 17:33:40 +0200

    Add project files.

---
+++ Lab1/Controllers/CarsController.cs
@@ -0,0 +1,25 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public enum Status
+    {
+        table,
+        list
+    }
+    public class CarsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            var cars = Car.GetCars();
+            return View(cars);
+        }
+        public IActionResult GetDetail(string img, Status status)
+        {
+            var car = Car.GetCars().FirstOrDefault(c => c.Image == img);
+            var data = new { car, status }; //Anonymous Object
+            return View(data);
+        }
+    }
+}
